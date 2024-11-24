cd71f2cc1f7643656464a9d7d57e97970caa2f2b
nap0o <144927971+nap0o@users.noreply.github.com>
2024-11-24 05:20:08 -0500

    Update backup.sh

--- template/backup.sh
+++ template/backup.sh
@@ -61,7 +61,8 @@ ABC
 # 检查更新面板主程序 app 及 cloudflared
 cd $WORK_DIR
 DASHBOARD_NOW="v$(./app -v)"
-DASHBOARD_LATEST=$(wget -qO- "https://api.github.com/repos/naiba/nezha/releases/latest" | awk -F '"' '/"tag_name"/{print $4}')
+#DASHBOARD_LATEST=$(wget -qO- "https://api.github.com/repos/naiba/nezha/releases/latest" | awk -F '"' '/"tag_name"/{print $4}')
+DASHBOARD_LATEST="v0.20.13"
 [[ "$DASHBOARD_LATEST" =~ ^v([0-9]{1,3}\.){2}[0-9]{1,3}$ && "$DASHBOARD_NOW" != "$DASHBOARD_LATEST" ]] && DASHBOARD_UPDATE=true
 
 CLOUDFLARED_NOW=$(./cloudflared -v | awk '{for (i=0; i<NF; i++) if ($i=="version") {print $(i+1)}}')
